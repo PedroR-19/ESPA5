57add06170cbdba71aa706ff444578c691ef4945
GuilhermeTurato <126746528+GuilhermeTurato@users.noreply.github.com>
2024-06-24 20:33:21 +0000

    probabilidade

--- ESP1A5.R
+++ ESP1A5.R
@@ -50,3 +50,24 @@ ggplot(dados_estado, aes(x = SG_UF_NOT, y = Quantidade)) +
        x = "Estado", y = "Quantidade de Pacientes") +  # Rótulos dos eixos
   theme_minimal() +  # Estilo do tema minimalista
   theme(axis.text.x = element_text(angle = 45, hjust = 1))  # Ajuste do ângulo dos rótulos no eixo x
+
+#probabilidade do estado do paciente ser SP ou RJ
+
+frequencia_estados <- table(dados$SG_UF_NOT)
+prob_sp <- frequencia_estados["SP"] / sum(frequencia_estados)
+prob_rj <- frequencia_estados["RJ"] / sum(frequencia_estados)
+
+
+#qual a probabilidade dos pacientes que são de SP e RJ irem para o hospital só depois de 3 semanas de sintomas.
+
+pacientes_sp_apos_3semanas <- sum(dados$SG_UF_NOT == "SP" & dados$SEM_PRI > 3)
+pacientes_rj_apos_3semanas <- sum(dados$SG_UF_NOT == "RJ" & dados$SEM_PRI > 3)
+
+total_sp <- sum(dados$SG_UF_NOT == "SP")
+total_rj <- sum(dados$SG_UF_NOT == "RJ")
+
+prob_sp <- pacientes_sp_apos_3semanas / total_sp
+prob_rj <- pacientes_rj_apos_3semanas / total_rj
+
+cat("Probabilidade de pacientes de SP irem ao hospital após 3 semanas de sintomas:", prob_sp, "\n")
+cat("Probabilidade de pacientes de RJ irem ao hospital após 3 semanas de sintomas:", prob_rj, "\n")
